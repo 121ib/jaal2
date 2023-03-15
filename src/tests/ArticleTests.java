366a47e8f1111bbe095eb0b4a922d542eef119db
Биялиев Изат <bialievizat@MacBook-Pro-Bialiev.local>
2023-03-15 13:48:50 +0600

    l5_ex9

---
+++ src/tests/ArticleTests.java
@@ -0,0 +1,38 @@
+package tests;
+
+import lib.CoreTestCase;
+import lib.ui.ArticlePageObject;
+import lib.ui.SearchPageObject;
+import org.junit.Test;
+
+public class ArticleTests extends CoreTestCase {
+    @Test
+    public void testCompareArticleTitle() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Java");
+        SearchPageObject.clickArticleWithSubstring("Object-oriented programming language");
+        ArticlePageObject ArticlePageObject = new ArticlePageObject(driver);
+        String article_title = ArticlePageObject.getArticleTitle();
+
+        assertEquals(
+                "We see unexpect title!",
+                "Java (programming language)",
+                article_title
+        );
+    }
+
+    @Test
+    public void testSwipeArticle() {
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Appium");
+        SearchPageObject.clickArticleWithSubstring("Appium");
+
+        ArticlePageObject ArticlePageObject = new ArticlePageObject(driver);
+        ArticlePageObject.waitForTitleElement();
+        ArticlePageObject.swipeToFooter();
+    }
+
+
+}
